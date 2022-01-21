dc777e5442cd7a737be10994d5e1b72595bb3537
Anatoly Moskalev <moskalev1997tolya@gmail.com>
2022-01-21 22:13:24 +0600

    added StatisticsService and StatisticsServiceTest

---
+++ src/main/java/ru/netology/stats/StatisticsService.java
@@ -0,0 +1,20 @@
+package ru.netology.stats;
+
+public class StatisticsService {
+
+    /**
+     * Calculate index of max income
+     *
+     * @param incomes - array of incomes
+     * @return - index of first max value
+     */
+    public long findMax(long[] incomes) {
+        long current_max_index = 0;
+        long current_max = incomes[0];
+        for (long income : incomes)
+            if (current_max < income)
+                current_max = income;
+        System.out.println(current_max);
+        return current_max;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatisticsServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 8};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+
+    }
+}
